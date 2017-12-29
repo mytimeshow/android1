4b9f2db23763958e02d511c8e8136c89aab96990
zhanwen <zhanwen@gmail.com>
2017-12-29 18:15:15 +0800

    降价拍界面的修改

--- app/src/main/java/cn/czyugang/tcg/client/entity/ReduceProduct.java
+++ app/src/main/java/cn/czyugang/tcg/client/entity/ReduceProduct.java
@@ -2,94 +2,64 @@ package cn.czyugang.tcg.client.entity;
 
 import com.google.gson.annotations.SerializedName;
 
-import java.util.List;
-
 /**
  * Created by Administrator on 2017/12/25 0025.
  */
 
 public class ReduceProduct {
 
-
     /**
-     * assessmentCount : 0
-     * enableBuyCount : 0
-     * groupList : [{"currentPrice":0,"id":"string","name":"string","restTime":0}]
-     * groupTime : 0
-     * labelList : [{"count":0,"id":"string","name":"string"}]
-     * minPrice : 0
-     * productPicIdList : ["string"]
-     * productPrice : 0
-     * productStoreId : string
-     * productStoreInventoryId : string
-     * productSubTitle : string
-     * productTitle : string
-     * reducePrice : 0
+     * bookType : string
+     * createTime : 2017-12-29T03:36:56.561Z
+     * deleteFlag : string
+     * disparage : 0
+     * id : string
+     * order : 0
+     * praise : 0
+     * productId : string
+     * qrCodeUrl : string
+     * recycled : string
+     * saleTimeType : string
      * sales : 0
      * score : 0
+     * status : string
+     * statusUpdateTime : 2017-12-29T03:36:56.561Z
+     * storeId : string
+     * updateTime : 2017-12-29T03:36:56.561Z
      */
 
-    @SerializedName("assessmentCount")
-    public int assessmentCount;
-    @SerializedName("enableBuyCount")
-    public int enableBuyCount;
-    @SerializedName("groupTime")
-    public int groupTime;
-    @SerializedName("minPrice")
-    public double minPrice;
-    @SerializedName("productPrice")
-    public double productPrice;
-    @SerializedName("productStoreId")
-    public String productStoreId;
-    @SerializedName("productStoreInventoryId")
-    public String productStoreInventoryId;
-    @SerializedName("productSubTitle")
-    public String productSubTitle;
-    @SerializedName("productTitle")
-    public String productTitle;
-    @SerializedName("reducePrice")
-    public double reducePrice;
+    @SerializedName("bookType")
+    public String bookType;
+    @SerializedName("createTime")
+    public String createTime;
+    @SerializedName("deleteFlag")
+    public String deleteFlag;
+    @SerializedName("disparage")
+    public int disparage;
+    @SerializedName("id")
+    public String id;
+    @SerializedName("order")
+    public int order;
+    @SerializedName("praise")
+    public int praise;
+    @SerializedName("productId")
+    public String productId;
+    @SerializedName("qrCodeUrl")
+    public String qrCodeUrl;
+    @SerializedName("recycled")
+    public String recycled;
+    @SerializedName("saleTimeType")
+    public String saleTimeType;
     @SerializedName("sales")
     public int sales;
     @SerializedName("score")
     public double score;
-    @SerializedName("groupList")
-    public List<GroupListBean> groupList;
-    @SerializedName("labelList")
-    public List<LabelListBean> labelList;
-    @SerializedName("productPicIdList")
-    public List<String> productPicIdList;
-
-    public static class GroupListBean {
-        /**
-         * currentPrice : 0
-         * id : string
-         * name : string
-         * restTime : 0
-         */
-
-        @SerializedName("currentPrice")
-        public double currentPrice;
-        @SerializedName("id")
-        public String id;
-        @SerializedName("name")
-        public String name;
-        @SerializedName("restTime")
-        public int restTime;
-    }
-
-    public static class LabelListBean {
-        /**
-         * count : 0
-         * id : string
-         * name : string
-         */
-
-        @SerializedName("count")
-        public int count;
-        @SerializedName("id")
-        public String id;
-        @SerializedName("name")
-        public String name;
-    }
+    @SerializedName("status")
+    public String status;
+    @SerializedName("statusUpdateTime")
+    public String statusUpdateTime;
+    @SerializedName("storeId")
+    public String storeId;
+    @SerializedName("updateTime")
+    public String updateTime;
 }
